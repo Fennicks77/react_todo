6ee68925a30b2641ed0929ee8b2b409fd3432761
Nick <nituev7@gmail.com>
2015-02-04 22:34:43 +0300

    onclick and items

--- build/app.js
+++ build/app.js
@@ -1,34 +1,55 @@
 var ToDoListItem = React.createClass({displayName: "ToDoListItem",
 	getInitialState: function(){
 		return {
-			done: false
+			done: this.props.done
 		};
 	},
 	onDoneChange: function(e){
-		this.setState({done: !e.target.checked});
+		this.setState({done: !this.state.done});
 	},
 	render: function(){
-		return React.createElement("li", null, 
-			React.createElement("input", {type: "checkbox", id: this.props.id, className: "cb-done", defaultChecked: this.state.done, onChange: this.onDoneChange}), 
-			React.createElement("label", {htmlFor: this.props.id}, this.props.title)
-		); 
+		return (
+			React.createElement("li", null, 
+				React.createElement("input", {type: "checkbox", id: this.props.id, className: "cb-done", checked: this.state.done, onChange: this.onDoneChange}), 
+				React.createElement("label", {htmlFor: this.props.id}, this.props.title)
+			)); 
 	}
 }) 
 var ToDoList = React.createClass({displayName: "ToDoList",
+	getInitialState: function(){
+		return {
+			input: '',
+			items: []
+		};
+	},
+	onInput: function(e){
+		this.setState({input: e.target.value})
+	},
+	onAddClick: function(e){
+		var item = {};
+		item.done=false;
+		item.title=this.state.input;
+		var items = this.state.items;
+		items.push(item);
+		this.setState({items: items, input:''});
+	},
+	renderItem: function(item){
+		return (React.createElement(ToDoListItem, {id: 0, title: item.title, done: item.done}));
+	},
 	render: function(){
-		return React.createElement("div", {id: "main"}, 
-      		React.createElement("h1", {className: "h1"}, React.createElement("span", {className: "glyphicon glyphicon-th-list"}), " Todolist"), 
-      		React.createElement("div", {className: "form_wrapper input-group"}, 
-        		React.createElement("input", {type: "text", name: "123", placeholder: "What to do?", autofocus: true, required: true, size: "27", className: "form-control"}), 
-          		React.createElement("span", {className: "input-group-btn"}, 
-            		React.createElement("button", {className: "btn btn-primary"}, React.createElement("span", {className: "glyphicon glyphicon-ok"}))
-          		)
-      		), 
-       		React.createElement("ul", {className: "list"}, 
-       			React.createElement(ToDoListItem, {id: 0, title: 'Milk'}), 
-       			React.createElement(ToDoListItem, {id: 1, title: 'Cow'})
-       		)
-    		);
+		return(
+			React.createElement("div", {id: "main"}, 
+				React.createElement("h1", {className: "h1"}, React.createElement("span", {className: "glyphicon glyphicon-th-list"}), " Todolist"), 
+				React.createElement("div", {className: "form_wrapper input-group"}, 
+					React.createElement("input", {type: "text", placeholder: "What to do?", autofocus: true, className: "form-control", value: this.state.input, onChange: this.onInput}), 
+					React.createElement("span", {className: "input-group-btn"}, 
+						React.createElement("button", {className: "btn btn-primary", onClick: this.onAddClick}, React.createElement("span", {className: "glyphicon glyphicon-ok"}))
+					)
+				), 
+				React.createElement("ul", {className: "list"}, 
+					this.state.items.map(this.renderItem)
+				)
+			));
 	}
 });
 
--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,34 +1,55 @@
 var ToDoListItem = React.createClass({
 	getInitialState: function(){
 		return {
-			done: false
+			done: this.props.done
 		};
 	},
 	onDoneChange: function(e){
-		this.setState({done: !e.target.checked});
+		this.setState({done: !this.state.done});
 	},
 	render: function(){
-		return <li>
-			<input type="checkbox" id={this.props.id} className="cb-done" defaultChecked={this.state.done} onChange={this.onDoneChange} />
-			<label htmlFor={this.props.id}>{this.props.title}</label>
-		</li>; 
+		return (
+			<li>
+				<input type="checkbox" id={this.props.id} className="cb-done" checked={this.state.done} onChange={this.onDoneChange} />
+				<label htmlFor={this.props.id}>{this.props.title}</label>
+			</li>); 
 	}
 }) 
 var ToDoList = React.createClass({
+	getInitialState: function(){
+		return {
+			input: '',
+			items: []
+		};
+	},
+	onInput: function(e){
+		this.setState({input: e.target.value})
+	},
+	onAddClick: function(e){
+		var item = {};
+		item.done=false;
+		item.title=this.state.input;
+		var items = this.state.items;
+		items.push(item);
+		this.setState({items: items, input:''});
+	},
+	renderItem: function(item){
+		return (<ToDoListItem id={0} title={item.title} done={item.done}/>);
+	},
 	render: function(){
-		return <div id="main"> 
-      		<h1 className="h1"><span className="glyphicon glyphicon-th-list" /> Todolist</h1>
-      		<div className="form_wrapper input-group">
-        		<input type="text" name="123" placeholder="What to do?" autofocus required size="27" className="form-control" />
-          		<span className="input-group-btn">
-            		<button className="btn btn-primary"><span className="glyphicon glyphicon-ok" /></button>
-          		</span>
-      		</div>    
-       		<ul className="list">
-       			<ToDoListItem  id={0} title={'Milk'} />
-       			<ToDoListItem  id={1} title={'Cow'} />
-       		</ul> 
-    		</div>;
+		return(
+			<div id="main"> 
+				<h1 className="h1"><span className="glyphicon glyphicon-th-list" /> Todolist</h1>
+				<div className="form_wrapper input-group">
+					<input type="text" placeholder="What to do?" autofocus className="form-control" value={this.state.input} onChange={this.onInput} />
+					<span className="input-group-btn">
+						<button className="btn btn-primary" onClick={this.onAddClick}><span className="glyphicon glyphicon-ok" /></button>
+					</span>
+				</div> 
+				<ul className="list">
+					{this.state.items.map(this.renderItem)}
+				</ul> 
+			</div>);
 	}
 });
 
